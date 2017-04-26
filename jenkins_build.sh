a2f7315002de99d54cf0b70ecfe9d6e85e1a5950
Linux User <Ernad husremovic>
2017-04-26 12:42:17 +0200

    /opt/rt-n56u/trunk

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -4,7 +4,7 @@ VOLUME_BASE=$(pwd)
 
 ./build_docker_image.sh
 
-sudo mkdir -p $VOLUME_BASE/source
+sudo mkdir -p $VOLUME_BASE/rt-n56u
 
 docker rm -f fwbuilder
 
@@ -17,7 +17,7 @@ fi
 docker run \
    -t \
    --name fwbuilder \
-   -v $VOLUME_BASE/source:/opt/source \
+   -v $VOLUME_BASE/source:/opt/rt-n56u \
    $vol_2 \
    fwbuilder /start.sh
 
--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ ! -d /opt/source/rt-n56u ] ; then
-   git clone https://bitbucket.org/padavan/rt-n56u.git
+if [ ! -d /opt/rt-n56u/trunk ] ; then
+   cd /opt/rt-n56u
+   git init
+   git remote add upstream https://bitbucket.org/padavan/rt-n56u.git
+   git fetch upstream
+   git merge upstream/master
+   
 else
    cd /opt/source/rt-n56u
+   git checkout -f master
    git pull
-   git checkout -f
 fi
    
