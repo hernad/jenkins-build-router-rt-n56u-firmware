f08eea4f12031b450821e1e9600d5e286b4bdbac
Linux User <Ernad husremovic>
2017-04-26 14:38:00 +0200

    default config

--- scripts/build_firmware.sh
+++ scripts/build_firmware.sh
@@ -2,7 +2,15 @@
 
 echo ========= build-firmware =====================
 
-cd /opt/rt-n56u/trunk
+TRUNK_DIR=/opt/rt-n56u/trunk
+KERNEL_DIR=$TRUNK_DIR/linux-3.4.x
+
+cd $KERNEL_DIR
+make savedefconfig 
+cp -av .config /opt/rt-n56u/kernel_default_config
+
+
+cd $TRUNK_DIR
 
 echo n56u_aria.config
 cp configs/templates/n56u_aria.config .config
@@ -12,17 +20,13 @@ if [ -f kernel.config ] ; then
 fi
 
 
+#cp build_firmware my_build_firmware
 
+#sed -e 's/make dep/echo dummy 1/' my_build_firmware -i
+#sed -e 's/make/echo dummy 2/' my_build_firmware -i
 
-cp build_firmware my_build_firmware
-
-sed -e 's/make dep/echo dummy 1/' my_build_firmware -i
-sed -e 's/make/echo dummy 2/' my_build_firmware -i
-
-echo 
- 
-
-. ./my_build_firmware
+#. ./my_build_firmware
+./build_firmware
 
 func_enable_kernel_param "CONFIG_NET_IPIP"
 #func_enable_kernel_param "CONFIG_NET_IPGRE_BROADCAST"
