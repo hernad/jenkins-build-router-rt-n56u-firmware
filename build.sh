04eb615b1270eeae790cd8d55a787d9eb7da2451
Linux User <Ernad husremovic>
2017-04-26 13:58:52 +0200

    check build toolchain

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 scripts/refresh_git.sh
-scripts/build_toolchain.sh
+if [ -f /opt/rt-n56u/toolchain-mipsel/toolchain-3.4.x/bin/mipsel-linux-uclibc-cpp ] ; then
+        echo "-- toolchain already built --"
+else
+        scripts/build_toolchain.sh
+fi
 scripts/build_firmware.sh
 
